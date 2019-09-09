9f08c2cae665668403263d1bc5ef7519bd2dcce5
Domenico <domedfd@gmail.com>
2019-09-09 12:03:04 -0400

    Utilizacion de la props y como funciona

---
+++ ex 3/componete.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => (<h1>Primeiro Componente!</h1>, <h2>arrow function</h2>);
--- ex 3/index.jsx
+++ ex 3/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Componente from "./componete";
 
-ReactDOM.render(<Componente value="Show!" />, document.getElementById("app"));
+ReactDOM.render(<Componente />, document.getElementById("app"));
