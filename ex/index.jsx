43321e8254790567edbb4b19ec7e7c04e9b71774
Domenico <domedfd@gmail.com>
2019-09-09 13:22:16 -0400

    Usando propiedades childern en un componente

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SilvaFamily from "./silvaFamily";
+import Family from "./family";
+import Member from "./member";
 
-ReactDOM.render(<SilvaFamily />, document.getElementById("app"));
+ReactDOM.render(
+  <Family>
+    <Member name="Guilherme" lastName="Silva" />
+  </Family>,
+  document.getElementById("app")
+);
