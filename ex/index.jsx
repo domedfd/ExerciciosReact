e817f849c5d134d504157d63da0ce0170e6654d2
Domenico <domedfd@gmail.com>
2019-09-10 11:22:12 -0400

    integrando react no redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { combineReducers, createStore } from "redux";
 import { Provider } from "react-redux";
+
 import Field from "./field";
 
 const reducers = combineReducers({
